012c5b7e9b076e60f0b170ba048e640b0d786082
Tobias Begalke <tb@tb.unnet>
2013-02-22 09:53:09 +0100

    license changed to BSD, added documentation to README, added proxy code

--- lib/yboss/base.rb
+++ lib/yboss/base.rb
@@ -9,14 +9,18 @@ module YBoss
 
     attr_reader :credits, :options
 
-    def initialize(options = { })
+    def initialize(options = {})
+      if options.is_a?(Hash)
+        options.delete('format')
+      end
+
       @options = {
         'format' => 'json',
       }.merge(options)
 
-      if YBoss.config.proxy
-        RestClient.proxy = YBoss.config.proxy
-      end
+      @oauth = YBoss::Oauth.new
+      @oauth.consumer_key = YBoss::Config.instance.oauth_key
+      @oauth.consumer_secret = YBoss::Config.instance.oauth_secret
     end
 
     def call(options = {})
@@ -33,25 +37,35 @@ module YBoss
       url = base_uri + '?'
       url += @options.find_all { |k,v| ! v.nil? }.map { |k,v| "#{k}=#{URI.escape(v)}" }.join('&')
 
-      if ! @oauth
-        @oauth = YBoss::Oauth.new
-        @oauth.consumer_key = YBoss::Config.instance.oauth_key
-        @oauth.consumer_secret = YBoss::Config.instance.oauth_secret
-      end
-
       parsed_url = URI.parse(url)
       url += "?" + @oauth.sign(parsed_url).query_string
 
       data = nil
 
-      Net::HTTP.start( parsed_url.host ) { | http |
-        req = Net::HTTP::Get.new "#{ parsed_url.path }?" + @oauth.sign(parsed_url).query_string
-        response = http.request(req)
-        if ! response.is_a?(Net::HTTPSuccess)
-          raise FetchError.new("Got error while fetching #{url}: #{response.code} #{response.message}")
-        end
-        data = JSON.parse(response.read_body)
-      }
+      if YBoss::Config.instance.proxy
+        proxy_uri = URI.parse(YBoss::Config.instance.proxy)
+
+        Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port).start(parsed_url.host)  { | http |
+          req = Net::HTTP::Get.new "#{ parsed_url.path }?" + @oauth.sign(parsed_url).query_string
+          response = http.request(req)
+          if ! response.is_a?(Net::HTTPSuccess)
+            raise FetchError.new("Got error while fetching #{url}: #{response.code} #{response.message}")
+          end
+
+          data = JSON.parse(response.read_body)
+        }
+      else
+        Net::HTTP.start( parsed_url.host ) { | http |
+          req = Net::HTTP::Get.new "#{ parsed_url.path }?" + @oauth.sign(parsed_url).query_string
+          response = http.request(req)
+          if ! response.is_a?(Net::HTTPSuccess)
+            raise FetchError.new("Got error while fetching #{url}: #{response.code} #{response.message}")
+          end
+
+          data = JSON.parse(response.read_body)
+        }
+      end
+
 
       data
     end
@@ -59,7 +73,7 @@ module YBoss
     def method_name
       # derive the method name from the class name
       #
-      self.class.to_s.downcase.sub(/^.+?::/, '').gsub(/::/, '.')
+      self.class.to_s.downcase.split(/::/).last
     end
 
     def base_uri
